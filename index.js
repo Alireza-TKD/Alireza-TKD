61749107f98b70f7edb3fb1db1c86ed6dcfdd6bd
Alireza Jalali <jalali.ali99@gmail.com>
2021-03-29 12:24:32 +0430

    changed the whole JS

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// const namePar = () => {
+//     let par = document.getElementById('nameP') 
+//     let inp = document.getElementById('floatingName').value;
+//         par.innerHTML ='name :' + inp;
+// }
+const whole = () =>{
+    
+}
