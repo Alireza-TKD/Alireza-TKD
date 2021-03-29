c99d6d815050c621804fbc6974a5b2e059363f61
Alireza Jalali <jalali.ali99@gmail.com>
2021-03-29 13:13:41 +0430

    reseting inputs done by looping and is done

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-
-// const namePar = () => {
-//     let par = document.getElementById('nameP') 
-//     let inp = document.getElementById('floatingName').value;
-//         par.innerHTML ='name :' + inp;
-// }
+//whole evaluation
 const whole = () =>{
-    
+     document.querySelector('#eval').style.visibility = 'visible'
+     document.querySelector('#eval').style.animationName = 'fadeInUp' 
+     document.getElementById('nameP').innerHTML = 'name : ' + document.getElementById('floatingName').value;
+}
+
+//reseting
+const rese = () =>{
+     let allInp = document.querySelectorAll('.form-control');
+     let x ;
+     for(x in allInp){
+          allInp[x].value = ''
+     }
 }
